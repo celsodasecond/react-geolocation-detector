b607dbc0bd305f46f281c62353a03c9db6c0bd02
celsodasecond <75917932+celsodasecond@users.noreply.github.com>
2022-12-03 12:23:29 +0800

    ui changes

--- src/components/HemisphereDisplay.js
+++ src/components/HemisphereDisplay.js
@@ -1,15 +1,37 @@
 import React from 'react'
+
+// CSS
+import './HemisphereDisplay.css'
+
+// ASSETS
 import northernPic from '../assets/northern-hemisphere.png'
 import southernPic from '../assets/southern-hemisphere.png'
 
+const hemisphereConfig = {
+  Northern: {
+    text: 'You are in the Northern Hemisphere',
+    picture: northernPic
+  },
+  Southern: {
+    text: 'You are in the Southern Hemisphere',
+    picture: southernPic
+  }
+}
+
 export default function hemispheredisplay({ latitude }) { // ES 2015 Deconstruction Syntax
-  const hemisphere = latitude > 0 ? 'Northern Hemisphere' : 'Southern Hemisphere'
-  const picture = latitude > 0 ? northernPic : southernPic
+  const hemisphere = latitude > 0 ? 'Northern' : 'Southern'
+  const {text, picture} = hemisphereConfig[hemisphere]
 
   return(
-    <div>
-      <img src={picture} alt={hemisphere} />
-      You are in the {hemisphere}
+    <div className={hemisphere}>
+      <div className='ui raised text container segment'>
+        <div className='image'>
+          <img src={picture} alt={hemisphere} className='picture'/>
+        </div>
+        <div className='ui bottom attached label'>
+          <h1 className='hemisphereText'>{text}</h1>
+        </div>
+      </div>
     </div>
   )
 }
